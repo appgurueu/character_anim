3c66406bdc3429c251efcdeb456b246bc0c8b2a0
Lars Mueller <appgurulars@gmx.de>
2020-09-20 15:18:39 +0200

    Actual animating the character

---
+++ init.lua
@@ -0,0 +1,7 @@
+setfenv(1, modlib.mod)
+local namespace = create_namespace()
+local quaternion = setmetatable({}, {__index = namespace})
+include_env(get_resource"quaternion.lua", quaternion)
+namespace.quaternion = quaternion
+extend"importer"
+extend"main"
