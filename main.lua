d8e978243a4a9da83ba09dcbf104fc821fe0a0e8
Lars Mueller <appgurulars@gmx.de>
2022-05-11 19:47:20 +0200

    Fix incorrect total rotation

--- main.lua
+++ main.lua
@@ -235,7 +235,7 @@ function handle_player_animations(dtime, player)
 		local parent_rotation = attach_parent:get_rotation()
 		if attach_rotation and parent_rotation then
 			parent_rotation = vector.apply(parent_rotation, math.deg)
-			local total_rotation = normalize_rotation(vector.subtract(parent_rotation, attach_rotation))
+			local total_rotation = normalize_rotation(vector.add(parent_rotation, attach_rotation))
 			local function rotate_relative(euler_rotation)
 				-- HACK +180
 				euler_rotation.y = euler_rotation.y + look_horizontal + 180
